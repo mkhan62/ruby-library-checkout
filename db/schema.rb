81e121f34933565d2d6cd25bdccb992ce2aa11b8
Hamza Khan <mkhan62@hawk.iit.edu>
2018-02-12 18:18:28 -0600

    Ran the migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212234827) do
+ActiveRecord::Schema.define(version: 20180212235654) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "last_name"
+    t.string "first_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
